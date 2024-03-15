cfcbb48060dedb1c26e6ab7fca7f54a511f41e30
Saudii <isaudatu81@gmail.com>
2024-03-15 21:16:38 +0100

    def a str func to return the title of the model

--- todo_app/tasks/models.py
+++ todo_app/tasks/models.py
@@ -5,3 +5,5 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
