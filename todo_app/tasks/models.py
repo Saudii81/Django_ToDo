40bbf0f94d832dd5217ee20b4c9ac9581a7caa70
Saudii <isaudatu81@gmail.com>
2024-03-15 21:10:49 +0100

    create a new varible to check the time of creation of data

--- todo_app/tasks/models.py
+++ todo_app/tasks/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
