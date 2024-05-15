e38966595ee071a6aaee0896b2cf191a936bdd49
Saudii <isaudatu81@gmail.com>
2024-05-15 08:34:24 +0100

    makes route for edit urls

--- todo_app/tasks/urls.py
+++ todo_app/tasks/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("add/", views.add_task, name="add_task"),
-    
+    path("edit/<int:pk>/", views.edit_task, name="edit_task"),
 ]
