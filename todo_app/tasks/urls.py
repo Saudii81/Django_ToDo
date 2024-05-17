0c3b6976e39ff58e43e1face8f1451c98d51e10b
Saudii <isaudatu81@gmail.com>
2024-05-17 08:43:59 +0100

    creates a delete url path in the urlpatterens list

--- todo_app/tasks/urls.py
+++ todo_app/tasks/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("add/", views.add_task, name="add_task"),
     path("edit/<int:pk>/", views.edit_task, name="edit_task"),
+    path("delete/<int:pk>/", views.delete_task, name="delete_task"),
 ]
